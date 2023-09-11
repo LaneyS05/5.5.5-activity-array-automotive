923c02d99bf8186f21d1d0358db99f2acc0ed0f7
LaneyS05 <laney.staggs05@gmail.com>
2023-09-11 19:30:18 -0400

    array-automotive

--- factory.js
+++ factory.js
@@ -1,3 +1,20 @@
 class Factory{
-    
+    constructor(location, owner){
+        this.location = location 
+        this.owner = owner
+        this.vehicles = []
+    }
+    createVehicle(vehicle){
+        this.vehicles.push(vehicle)
+    }
+    paintVehicle(vin, painColor){
+        let vIndex = this.vehicles.findIndex(v => v.vin === vin)
+        this.vehicles[vIndex].paint(painColor)
+    }
+    listVehicles(){
+        return this.vehicles
+    }
 }
+
+
+
